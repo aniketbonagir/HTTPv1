bc2bb5ce079c26d40b5b845540ecd9e792e5aeb8
aniketbonagir <aniketbonagir@hotmail.com>
2019-04-22 10:26:35 +0530

    First Version of HTTP example
    First Version of HTTP example

---
+++ src/components/FullPost/FullPost.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './FullPost.css';
+
+class FullPost extends Component {
+    state = {
+        postObj: null
+    }
+
+    componentDidUpdate() {
+        
+        if(this.props.id) {
+            if (!this.state.postObj || (this.state.postObj && this.state.postObj.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(resp => {
+                    this.setState({ postObj: resp.data });
+                })
+            }           
+        }
+    }
+
+    render () {
+        let post = <p style={{textAlign: "center"}}>Please select a Post!</p>;
+        if (this.props.id) {
+            post = <p style={{ textAlign: "center" }}>Loading Post!</p>;
+        }
+        if (this.state.postObj) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.postObj.title}</h1>
+                    <p>{this.state.postObj.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+
+            );
+        }
+        
+        return post;
+    }
+}
+
+export default FullPost;
